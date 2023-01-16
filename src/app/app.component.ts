6717ec4a42042e5ce6c57dddcae7a337cfcd2370
jhonat23 <jhonat23@gmail.com>
2023-01-16 11:35:03 -0500

    finally, a responsive interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'recipes_generator';
+  title: string = 'recipes_generator';
+  menuDisabled: boolean = true;
 }
