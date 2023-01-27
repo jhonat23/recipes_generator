7dd8695903f176998592373481020165aace0de5
jhonat23 <jhonat23@gmail.com>
2023-01-26 21:19:22 -0500

    finally connected with api, pending of fastapi and model prompt ajustement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,35 @@ import { OpenaiService } from '../app/services/openai.service';
 })
 export class AppComponent implements OnInit {
   title: string = 'recipes_generator';
-  menuDisabled: boolean = true;
-  example_recipe: any;
 
   constructor(
     private fastapi: OpenaiService
-  ) {
+  ) {}
 
-  }
+  menuDisabled: boolean = true;
+  example_recipe: any;
+  recipe: any = '\u{1F9C6}';
+  ingridients: any;
+  temp: any;
+
 
   ngOnInit(): void {
-
+    this.fastapi.getExampleRecipe().subscribe(data => {
+      console.log(data);
+      this.example_recipe = data;
+    })
   }
 
   obtainExampleRecipe() {
-    this.fastapi.getExampleRecipe().subscribe(data => {
+    console.log(this.recipe);
+    console.log(this.ingridients);
+  }
+
+  obtainRecipe() {
+    this.fastapi.getRecipe(this.ingridients).subscribe(data => {
       console.log(data);
+      this.temp = data;
+      this.recipe = this.temp.instructions;
     })
   }
 
--- src/app/services/openai.service.ts
+++ src/app/services/openai.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpenaiService {
+
+headers: HttpHeaders = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
+params: HttpParams = new HttpParams();
 
   constructor(
     private api: HttpClient
@@ -12,5 +15,10 @@ export class OpenaiService {
 
   getExampleRecipe(){
     return this.api.get('https://recipe-generator-api.onrender.com/example-recipe');
+  }
+
+  getRecipe(recipe: string) {
+    this.params = this.params.append('recipe', recipe);
+    return this.api.post('https://recipe-generator-api.onrender.com/recipe', this.params, {headers: this.headers});
   }
 }
